8337a655d1c569ba6c5fdcb48ddf9c0865b2db90
sukantahui <huisukanta77@gmail.com>
2021-06-22 18:42:09 +0530

    attribute created

--- metal_api/app/Models/ProductCategory.php
+++ metal_api/app/Models/ProductCategory.php
@@ -12,6 +12,7 @@ class ProductCategory extends Model
     protected $hidden = [
         "created_at","updated_at"
     ];
+    protected $appends = ['product_count'];
     /*Suppose you have a variable in database as tinyint or int but you want use it in laravel as
     boolean then cast the field as below to get it as boolean
     */
@@ -22,5 +23,10 @@ class ProductCategory extends Model
     public function products()
     {
         return $this->hasMany('App\Models\Product','product_category_id');
+    }
+    //this way we can create attribute in model
+    public function getProductCountAttribute()
+    {
+        return $this->products()->count();
     }
 }
